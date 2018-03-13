ad075e9120dd237c9b0c4174c15e416e0a3c3007
JETBLACKTHREATGOKUBLACK <32076581+JETBLACKTHREATGOKUBLACK@users.noreply.github.com>
2018-03-13 11:01:25 -0700

    Update StringExplorer.java

--- MagpieActivityStarterCode/activity3/StringExplorer.java
+++ MagpieActivityStarterCode/activity3/StringExplorer.java
@@ -7,21 +7,28 @@
 public class StringExplorer
 {
 
-	public static void main(String[] args)
-	{
-		String sample = "The quick brown fox jumped over the lazy dog.";
-		
-		//  Demonstrate the indexOf method.
-		int position = sample.indexOf("quick");
-		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
-		//  Demonstrate the toLowerCase method.
-		String lowerCase = sample.toLowerCase();
-		System.out.println ("sample.toLowerCase() = " + lowerCase);
-		System.out.println ("After toLowerCase(), sample = " + sample);
-		
-		//  Try other methods here:
-
-	}
+ public static void main(String[] args)
+ {
+  String sample = "The quick brown fox jumped over the lazy dog.";
+  
+  int notFoundPsn = sample.indexOf("slow");
+  System.out.println ("sample.indexOf(\"slow\") = " + notFoundPsn);
+  
+  //  Demonstrate the indexOf method.
+  int position = sample.indexOf("quick");
+  System.out.println ("sample.indexOf(\"quick\") = " + position);
+  
+  //  Demonstrate the toLowerCase method.
+  String lowerCase = sample.toLowerCase();
+  System.out.println ("sample.toLowerCase() = " + lowerCase);
+  System.out.println ("After toLowerCase(), sample = " + sample);
+  
+  //  Try other methods here:
+  
+  //indexOf(String str) differs from indexOf(String str, int fromIndex) because
+       //they have different paramenters. One will start at a certain location while
+       //the other starts at index 0
+ }
+ 
 
 }
